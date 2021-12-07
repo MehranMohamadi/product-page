6b254b42ca6b3ee9937e81c7239f8a54073404e3
Mehran Mohammadi <mohammadi000098@gmail.com>
2021-12-07 23:39:11 +0330

    first style

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="container w-container w-container flex justify-center xl:pl-xl xl:pt-xl_top ">
+    <div class="flex flex-row flex-wrap  ">
+      <div class=" bg-blue-800 h-20 w-nav h-nav ">
+        1
+      </div>
+      <div class="h-products w-3/4 bg-red-300 grid grid-cols-4 gap-8  ">
+        <div class="bg-green-500">1</div>
+        <div class="bg-green-500">2</div>
+        <div class="bg-green-500">3</div>
+        <div class="bg-green-500">4</div>
+        <div class="bg-green-500">5</div>
+        <div class="bg-green-500">6</div>
+        <div class="bg-green-500">7</div>
+        <div class="bg-green-500">8</div>
+        <div class="bg-green-500">9</div>
+        <div class="bg-green-500">10</div>
+        <div class="bg-green-500">11</div>
+        <div class="bg-green-500">12</div>
+      </div>
+      <div class="h-products w-1/4 bg-black xl:p">3</div>
+    </div>
   </div>
-  <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
